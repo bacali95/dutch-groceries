1f265420fb437a2a90360983a7fabb3a639f708a
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2025-04-25 23:50:09 +0200

    Fix ESLint and Prettier configuration

---
+++ eslint.config.js
@@ -0,0 +1,66 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  // Apply to all files
+  {
+    ignores: [
+      // dependencies
+      'node_modules/**',
+      '.pnp.*',
+      '.yarn/**',
+
+      // production
+      'build/**',
+
+      // misc
+      '.DS_Store',
+      '.env*',
+
+      // configuration files
+      'vite.config.ts',
+      'tailwind.config.js',
+    ],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        document: 'readonly',
+        navigator: 'readonly',
+        window: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
